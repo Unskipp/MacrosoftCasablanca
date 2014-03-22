eb91fbcec7fc65cc41245834676d4353b827deca
Unskipp <Unskipp@Unskipp-PC>
2014-03-22 15:58:20 +0100

    Created methods for DBFacade

--- src/Datasource/DBFacade.java
+++ src/Datasource/DBFacade.java
@@ -5,7 +5,13 @@
  */
 package Datasource;
 
+import Domain.Clients;
+import Domain.Employees;
+import Domain.Facilities;
+import Domain.Reservations;
+import Domain.Rooms;
 import java.sql.Connection;
+import java.util.ArrayList;
 
 /**
  *
@@ -16,6 +22,8 @@ public class DBFacade
 
     private Connection connection;
     private static DBFacade instance;
+    private DataMapper map;
+            
     private DBFacade()
     {
         connection = new DBConnector().getConnection();
@@ -31,4 +39,54 @@ public class DBFacade
     }
     
     //INSERT THE METHODS HERE
+    
+    public ArrayList<Employees> getEmployeessBy (String searchCriteria, String value)
+    {
+        return map.getEmployeessBy(connection, searchCriteria, value);
+    }
+    
+    public ArrayList<Employees> getAllEmployeess()
+    {
+        return map.getAllEmployeess(connection);
+    }
+    
+    public ArrayList<Rooms> getRoomssBy(String searchCriteria, String value)
+    {
+        return map.getRoomssBy(connection, searchCriteria, value);
+    }
+    
+    public ArrayList<Rooms> getAllRoomss()
+    {
+        return map.getAllRoomss(connection);
+    }
+    
+    public ArrayList<Clients> getClientssBy(String searchCriteria, String value)
+    {
+        return map.getClientssBy(connection, searchCriteria, value);
+    }
+    
+    public ArrayList<Clients> getAllClientss()
+    {
+        return map.getAllClientss(connection);
+    }
+    
+    public ArrayList<Reservations> getReservationssBy(String searchCriteria, String value)
+    {
+        return map.getReservationssBy(connection, searchCriteria, value);
+    }
+    
+     public ArrayList<Reservations> getAllReservationss()
+     {
+         return map.getAllReservationss(connection);
+     }
+     
+     public ArrayList<Facilities> getFacilitiesBy(String searchCriteria, String value)
+     {
+         return map.getFacilitiesBy(connection, searchCriteria, value);
+     }
+     
+     public ArrayList<Facilities> getAllFacilities()
+     {
+         return map.getAllFacilities(connection);
+     }
 }
